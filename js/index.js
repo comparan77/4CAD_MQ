271c8d5806c13b04e6a4f829b6b890987eda1ee3
cancer <comparan77@gmail.com>
2017-12-06 15:29:42 -0600

    global file to save global var

--- js/index.js
+++ js/index.js
@@ -19,13 +19,6 @@
 
 //var lstTarima = [];
 //var urlHandler = 'https://servidor.casc.com.mx/';
-var urlHandler = 'http://yaaxil.ddns.net:83/';
-
-var oUsuario;
-var oCADController;
-var menuAct = 'inicio';
-
-var oIndexCtrl;
 
 var app = {
     // Application Constructor
