1be28671997f3cdd7ad9b27c443120a2c1979043
cancer <comparan77@gmail.com>
2018-02-22 09:55:08 -0600

    se valida que las OT contengan servicios

--- js/controller/desordController.js
+++ js/controller/desordController.js
@@ -148,7 +148,12 @@ var DesOrdController = function() {
 				return obj.Folio == folio;
 			});
 		}
-		return itemOT.length > 0 ? itemOT[0] : null;
+		if(itemOT.length != undefined && itemOT.length > 0) {
+			if(itemOT.PLstOTSer.length > 0)
+				return itemOT[0];
+			else return null;
+		}
+		//return itemOT.length > 0 ? itemOT[0] : null;
 	}
 
 	function find_ordenes_maquilados(codliver) {
